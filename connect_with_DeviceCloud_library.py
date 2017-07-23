a49f468193476ab3e2ccebf9c186b15dd1038c96
yevgheniy <yevgeniy.sakhno@gmail.com>
2017-07-23 17:33:16 +0300

    Beginning to write own connection datastream class

--- connect_with_DeviceCloud_library.py
+++ connect_with_DeviceCloud_library.py
@@ -1,4 +1,5 @@
 from devicecloud import DeviceCloud
+import pprint
 
 # The following lines require manual changes
 username = "nanri-test" # enter your username
@@ -27,6 +28,9 @@ print
 print "== All our streams =="
 for stream in dc.streams.get_streams():
     print "%s " % (stream.get_stream_id())
+    raw_data_from_devicecloud = list(stream.read())
+    print 'This stream contains {} datapoints'.format(len(raw_data_from_devicecloud))
+    pprint.pprint(raw_data_from_devicecloud)
     print
 
 # get the name and current value of all data streams having values
@@ -37,4 +41,7 @@ print "== Our digit streams =="
 for stream in dc.streams.get_streams():
     if stream.get_data_type().lower() in ('float', 'double'):
         print "%s -> %s" % (stream.get_stream_id(), stream.get_current_value())
-        print
+        raw_data_from_devicecloud = list(stream.read())
+        # for dc in raw_data_from_device cloud:
+        print("Read {} data points.".format(len(raw_data_from_devicecloud)))
+    print
