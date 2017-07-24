86f250fdc8125f6f15b58f707041c45294bfd671
Yevgheniy <yevgeniy.sakhno@gmail.com>
2017-07-24 16:41:34 +0300

    Init function of class POJO

--- POJO_holding.py
+++ POJO_holding.py
@@ -3,9 +3,21 @@ import DataStreamConnect
 
 class POJO_holding:
     """Our own class, which get and store information from DeviceCloud stream"""
-    def __init__(self):
-        pass
 
+    def __init__(self, data, stream_id=None, description=None, timestamp=None,
+                 quality=None, location=None, data_type=None, units=None, dp_id=None,
+                 customer_id=None, server_timestamp=None):
+        self._stream_id = None  # invariant: always string or None
+        self._data = None  # invariant: could be any type, with conversion applied lazily
+        self._description = None  # invariant: always string or None
+        self._timestamp = None  # invariant: always datetime object or None
+        self._quality = None  # invariant: always integer (32-bit) or None
+        self._location = None  # invariant: 3-tuple<float> or None
+        self._data_type = None  # invariant: always string in set of types or None
+        self._units = None  # invariant: always string or None
+        self._dp_id = None  # invariant: always string or None
+        self._customer_id = None  # invariant: always string or None
+        self._server_timestamp = None  # invariant: always None or datetime
 
     def set_stream(self):
         pass
