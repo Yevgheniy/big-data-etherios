d76612f8f6a8f9986f6fb1fd901021004d83a061
Yevgheniy <yevgeniy.sakhno@gmail.com>
2017-07-24 14:29:56 +0300

    Beginning create class POJO

--- POJO_holding.py
+++ POJO_holding.py
@@ -3,3 +3,12 @@ import DataStreamConnect
 
 class POJO_holding:
     """Our own class, which get and store information from DeviceCloud stream"""
+    def __init__(self):
+        pass
+
+
+    def set_stream(self):
+        pass
+
+    def set_description(self):
+        pass
