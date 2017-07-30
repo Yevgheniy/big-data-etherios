1771eb2947e78a50dbe6cb2d0a2304657d734e00
yevgheniy <yevgeniy.sakhno@gmail.com>
2017-07-30 22:53:57 +0300

    Creating test_stream and datapoints is successfull. So, begining to create bd structure and code to put data to db.

--- my_test_stream_creating.py
+++ my_test_stream_creating.py
@@ -1,63 +1,43 @@
 from devicecloud import DeviceCloud
-import math
+from devicecloud.streams import DataPoint
+import random
 import time
 
-username = "11nanri-test" # our username
-password = "!Nanri0118" # our password
 
-dc = DeviceCloud(username, password)
-
-def write_points_and_delete_some(dc):
+def write_stream_and_points(dc):
+    # get a test stream reference
     test_stream = dc.streams.get_stream_if_exists("temperature_test_stream")
 
-    if test_stream is not None:
-        test_stream.delete()
-
-    # get a test stream reference
-    test_stream = dc.streams.get_stream_if_exists("test")
-
-    # we want a clean stream to work with.  If the stream exists, nuke it
+    # I can create only one stream, so delete it before creating new.
     if test_stream is not None:
         test_stream.delete()
 
     test_stream = dc.streams.create_stream(
-        stream_id="test",
-        data_type='float',
-        description='a stream used for testing',
-        units='some-unit',
+        stream_id="temperature_test_stream",
+        data_type='DOUBLE',
+        description='this stream used for testing',
+        data_ttl=8035200,
+        rollup_ttl=63244800,
+        units='C',
     )
 
-    print("Writing data points with five second delay")
-    for i in range(5):
-        print("Writing point {} / 5".format(i + 1))
+    print("Writing data points with two second delay")
+    for i in range(30):
+        print("Writing point {} ".format(i + 1))
         test_stream.write(DataPoint(
-            data=i * 1000,
-            description="This is {} * pi".format(i)
+            data = round(random.uniform(20,30),2),
+            description="this data points used for testing",
+            # location - Kharkiv
+            location=(49.980556,36.252500,113)
         ))
-        if i < (5 - 1):
-            time.sleep(1)
+        time.sleep(2)
 
-    points = list(test_stream.read(newest_first=False))
-    print("Read {} data points, removing the first".format(len(points)))
-
-    # Remove the first
-    test_stream.delete_datapoint(points[0])
-    points = list(test_stream.read(newest_first=False))
-    print("Read {} data points, removing ones written in last 30 seconds".format(len(points)))
-
-    # delete the ones in the middle
-    test_stream.delete_datapoints_in_time_range(
-        start_dt=points[1].get_timestamp(),
-        end_dt=points[-1].get_timestamp()
-    )
-    points = list(test_stream.read(newest_first=False))
-    print("Read {} data points.  Will try to delete all next".format(len(points)))
-    pprint.pprint(points)
-
-    # let's try without any range at all and see if they all get deleted
-    test_stream.delete_datapoints_in_time_range()
     points = list(test_stream.read(newest_first=False))
     print("Read {} data points".format(len(points)))
 
-    test_stream.delete()
 
+username = "nanri-test" # our username
+password = "!Nanri0118" # our password
+
+dc = DeviceCloud(username, password)
+write_stream_and_points(dc)
